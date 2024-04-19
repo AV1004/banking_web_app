7ef9626f81a1abd7afabd98428b7f77de852cad6
Valay Andhariya <andhariyavalay4@gmail.com>
2024-04-20 02:07:13 +0530

    Adding Transcation history page

---
+++ src/components/Dilog.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { TbListDetails } from "react-icons/tb";
+import { GrTransaction } from "react-icons/gr";
+import { FaDownload } from "react-icons/fa";
+
+import { Link } from "react-router-dom";
+
+export function NotificationDialog({ detail, type }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <>
+      <Button
+        size="sm"
+        variant="text"
+        className="flex items-center gap-2 text-[#222831] bg-[#EEEEEE] hover:bg-[#c6c6c6]"
+        onClick={handleOpen}
+      >
+        Details
+      </Button>
+      <Dialog open={open} handler={handleOpen} size="md">
+        <DialogHeader className="bg-[#222831]">
+          <Typography
+            variant="h5"
+            className="flex text-[#EEEEEE] bg-[#222831]"
+            color="blue-gray"
+          >
+            <TbListDetails className="mr-2 h-7 bg-[#222831]" />
+            Transaction Details
+          </Typography>
+        </DialogHeader>
+        <DialogBody divider className="grid place-items-center gap-4">
+          <GrTransaction className="h-28 w-28 text-[#222831]" />
+          <Typography className="text-[#222831]" variant="h1">
+            {detail.title}
+          </Typography>
+          <div className="text-gray-500 font-bold">
+            ____________________________________________________________________
+          </div>
+          <div className="flex  gap-32  w-full text-[#222831]">
+            <div className="ml-5 flex flex-col gap-2">
+              <Typography variant="h4">
+                {type === "sender" ? "Sender's" : "Receiver's"} Details
+              </Typography>
+              <Typography className="font-normal">
+                Name -&gt; {detail.name}
+              </Typography>
+              <Typography className="font-normal">
+                Account No -&gt; {detail.accountNo}
+              </Typography>
+              <Typography className="font-normal">
+                Moblie No -&gt; {detail.mobileNo}
+              </Typography>
+            </div>
+
+            <div className="gap-2 flex flex-col">
+              <Typography variant="h4">Date & Timing</Typography>
+              <Typography className="font-normal">
+                Date -&gt; {detail.date}
+              </Typography>
+              <Typography className="font-normal">
+                Time -&gt; {detail.time}
+              </Typography>
+
+              <Link>
+                <Button className="flex bg-[#222831] hover:bg-[#393E46]">
+                  Download Invoice{" "}
+                  <FaDownload className="ml-2 bg-[#222831] text-[#EEEEEE] hover:bg-[#454950]" />{" "}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </DialogBody>
+        <DialogFooter className="space-x-2">
+          <Button
+            variant="text"
+            color="blue-gray"
+            className="hover:bg-gray-500 text-gray-500 hover:text-[#222831]"
+            onClick={handleOpen}
+          >
+            close
+          </Button>
+          <Button
+            className="bg-[#222831] text-[#EEEEEE] hover:bg-[#454950]"
+            onClick={handleOpen}
+          >
+            Ok, Got it
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,302 @@
-import React from 'react'
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { PiArrowSquareDownRightLight } from "react-icons/pi";
+import { PiArrowSquareUpRightLight } from "react-icons/pi";
+import React, { useState } from "react";
+import { NotificationDialog } from "./Dilog";
 
 export default function History() {
+  // For Active Tab(i.e sent history or received history)
+  const [activeTab, setActiveTab] = useState("receive");
+  const changePaymentMethod = (method) => {
+    method === "receive" ? setActiveTab("receive") : setActiveTab("send");
+  };
+
+  // For dilog
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(!open);
+
+  // Dummy Data
+  const receivedMoney = [
+    {
+      title: "Borrowed",
+      name: "John Cardy",
+      amount: 2000,
+      accountNo: 121212121212,
+      mobileNo: 1234567890,
+      date: "12-12-2024",
+      time: "12:00PM",
+    },
+    {
+      title: "Lent",
+      name: "Alice Johnson",
+      amount: 1500,
+      accountNo: 987654321,
+      mobileNo: 9876543210,
+      date: "10-10-2024",
+      time: "10:00AM",
+    },
+    {
+      title: "Received",
+      name: "Bob Smith",
+      amount: 3000,
+      accountNo: 5555555555,
+      mobileNo: 5555555555,
+      date: "11-11-2024",
+      time: "11:00AM",
+    },
+    {
+      title: "Paid",
+      name: "Emily Brown",
+      amount: 2500,
+      accountNo: 4444444444,
+      mobileNo: 4444444444,
+      date: "09-09-2024",
+      time: "09:00AM",
+    },
+    {
+      title: "Donation",
+      name: "Michael Davis",
+      amount: 1800,
+      accountNo: 7777777777,
+      mobileNo: 7777777777,
+      date: "08-08-2024",
+      time: "08:00AM",
+    },
+    {
+      title: "Gift",
+      name: "Sophia Martinez",
+      amount: 2200,
+      accountNo: 6666666666,
+      mobileNo: 6666666666,
+      date: "07-07-2024",
+      time: "07:00AM",
+    },
+    {
+      title: "Salary",
+      name: "William Wilson",
+      amount: 1900,
+      accountNo: 3333333333,
+      mobileNo: 3333333333,
+      date: "06-06-2024",
+      time: "06:00AM",
+    },
+    {
+      title: "Investment",
+      name: "Olivia Anderson",
+      amount: 2700,
+      accountNo: 2222222222,
+      mobileNo: 2222222222,
+      date: "05-05-2024",
+      time: "05:00AM",
+    },
+    {
+      title: "Refund",
+      name: "James Garcia",
+      amount: 3200,
+      accountNo: 9999999999,
+      mobileNo: 9999999999,
+      date: "04-04-2024",
+      time: "04:00AM",
+    },
+  ];
+
+  const sentMoney = [
+    {
+      title: "Loan",
+      name: "James Garcia",
+      amount: 3099,
+      accountNo: 101010101010,
+      mobileNo: 9999999999,
+      date: "04-04-2024",
+      time: "04:00AM",
+    },
+    {
+      title: "Payment",
+      name: "Sophia Martinez",
+      amount: 2000,
+      accountNo: 2222222222,
+      mobileNo: 6666666666,
+      date: "05-05-2024",
+      time: "05:00PM",
+    },
+    {
+      title: "Transfer",
+      name: "Michael Davis",
+      amount: 1500,
+      accountNo: 3333333333,
+      mobileNo: 7777777777,
+      date: "06-06-2024",
+      time: "06:00PM",
+    },
+    {
+      title: "Donation",
+      name: "Olivia Anderson",
+      amount: 2500,
+      accountNo: 4444444444,
+      mobileNo: 8888888888,
+      date: "07-07-2024",
+      time: "07:00PM",
+    },
+    {
+      title: "Salary",
+      name: "William Wilson",
+      amount: 1800,
+      accountNo: 5555555555,
+      mobileNo: 9999999999,
+      date: "08-08-2024",
+      time: "08:00PM",
+    },
+    {
+      title: "Refund",
+      name: "Emily Brown",
+      amount: 2200,
+      accountNo: 6666666666,
+      mobileNo: 1111111111,
+      date: "09-09-2024",
+      time: "09:00PM",
+    },
+    {
+      title: "Expense",
+      name: "Bob Smith",
+      amount: 2700,
+      accountNo: 7777777777,
+      mobileNo: 2222222222,
+      date: "10-10-2024",
+      time: "10:00PM",
+    },
+    {
+      title: "Investment",
+      name: "Alice Johnson",
+      amount: 3000,
+      accountNo: 8888888888,
+      mobileNo: 3333333333,
+      date: "11-11-2024",
+      time: "11:00PM",
+    },
+    {
+      title: "Gift",
+      name: "John Cardy",
+      amount: 1900,
+      accountNo: 9999999999,
+      mobileNo: 4444444444,
+      date: "12-12-2024",
+      time: "12:00PM",
+    },
+  ];
+
   return (
-    <div>History</div>
-  )
+    <div className="mt-10 ml-10 mb-20">
+      <Button
+        className={` ${
+          activeTab === "receive"
+            ? "bg-[#222831] text-[#EEEEEE]  hover:bg-[#393E46]"
+            : ""
+        }  mr-4 `}
+        variant="text"
+        onClick={() => {
+          changePaymentMethod("receive");
+        }}
+      >
+        Received
+      </Button>
+      <Button
+        className={` ${
+          activeTab === "send"
+            ? "bg-[#222831] text-[#EEEEEE]  hover:bg-[#393E46]"
+            : ""
+        }   `}
+        variant="text"
+        onClick={() => {
+          changePaymentMethod("send");
+        }}
+      >
+        Sent
+      </Button>
+
+      {/* History */}
+      <div className="flex justify-center items-center gap-36 flex-wrap mt-10 mr-14">
+        {activeTab === "receive"
+          ? receivedMoney.map((receivedTranscation) => {
+              return (
+                <Card className="mt-6 w-96 bg-transparent" shadow={false}>
+                  <CardBody className="bg-[#222831] rounded-t-3xl text-[#EEEEEE]">
+                    <PiArrowSquareDownRightLight className="h-16 w-16 mb-3 bg-[#222831]" />
+                    <Typography
+                      variant="h6"
+                      color="blue-gray"
+                      className="-mb-3 bg-[#222831] text-[#EEEEEE]"
+                    >
+                      from
+                    </Typography>
+                    <Typography
+                      variant="h3"
+                      color="blue-gray"
+                      className="mb-2 bg-[#222831] text-[#EEEEEE]"
+                    >
+                      {receivedTranscation.name}
+                    </Typography>
+                    <div className="flex gap-4 bg-[#222831]">
+                      <Typography variant="h6" className=" bg-[#222831]">
+                        Amount: ₹{receivedTranscation.amount}
+                      </Typography>
+                      <Typography variant="h6" className=" bg-[#222831]">
+                        Title : {receivedTranscation.title}
+                      </Typography>
+                    </div>
+                  </CardBody>
+                  <CardFooter className="pt-0 bg-[#222831] ">
+                    <NotificationDialog
+                      type={"sender"}
+                      detail={receivedTranscation}
+                    />
+                  </CardFooter>
+                </Card>
+              );
+            })
+          : sentMoney.map((sentTranscation) => {
+              return (
+                <Card className="mt-6 w-96 bg-transparent" shadow={false}>
+                  <CardBody className="bg-[#222831] rounded-t-3xl text-[#EEEEEE]">
+                    <PiArrowSquareUpRightLight className="h-16 w-16 mb-3 bg-[#222831]" />
+                    <Typography
+                      variant="h6"
+                      color="blue-gray"
+                      className="-mb-3 bg-[#222831] text-[#EEEEEE]"
+                    >
+                      sent to
+                    </Typography>
+                    <Typography
+                      variant="h3"
+                      color="blue-gray"
+                      className="mb-2 bg-[#222831] text-[#EEEEEE]"
+                    >
+                      {sentTranscation.name}
+                    </Typography>
+                    <div className="flex gap-4 bg-[#222831]">
+                      <Typography variant="h6" className=" bg-[#222831]">
+                        Amount: ₹{sentTranscation.amount}
+                      </Typography>
+                      <Typography variant="h6" className=" bg-[#222831]">
+                        Title : {sentTranscation.title}
+                      </Typography>
+                    </div>
+                  </CardBody>
+                  <CardFooter className="pt-0 bg-[#222831] ">
+                    <NotificationDialog
+                      type={"receiver"}
+                      detail={sentTranscation}
+                    />
+                  </CardFooter>
+                </Card>
+              );
+            })}
+      </div>
+    </div>
+  );
 }
