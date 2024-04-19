8c3135089b9aacd8ecf4698eff497214c6427e8c
Valay Andhariya <andhariyavalay4@gmail.com>
2024-04-19 16:52:50 +0530

    Fixing Some Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./components/Error";
 import Root from "./components/Root";
-<<<<<<< HEAD
 import { lazy } from "react";
 import UserNav from "./components/UserNav";
 
@@ -9,21 +8,9 @@ const HomePage = lazy(() => import("./components/HomePage"));
 const SignUp = lazy(() => import("./components/SignUp"));
 const LogIn = lazy(() => import("./components/LogIn"));
 const UserHomePage = lazy(() => import("./components/UserHomePage"));
-const Transaction = lazy(() => import("./components/Transaction"));
+const MakeTransaction = lazy(() => import("./components/MakeTransaction"));
 const History = lazy(() => import("./components/History"));
 const UserProfile = lazy(() => import("./components/UserProfile"));
-=======
-import { lazy } from "react"
-import UserNav from "./components/UserNav";
-
-const HomePage=lazy(()=>import("./components/HomePage"));
-const SignUp=lazy(()=>import("./components/SignUp"));
-const LogIn=lazy(()=>import("./components/LogIn"));
-const UserHomePage=lazy(()=>import("./components/UserHomePage"));
-const Transaction=lazy(()=>import("./components/Transaction"));
-const History=lazy(()=>import("./components/History"));
-const UserProfile=lazy(()=>import("./components/UserProfile"));
->>>>>>> 823ca6338589d616903e1934a7fbd6136b748b31
 
 const router = createBrowserRouter([
   {
@@ -40,8 +27,8 @@ const router = createBrowserRouter([
     path: "/user",
     element: <UserNav />,
     children: [
-      { path: "home", element: <UserHomePage /> },
-      { path: "transaction", element: <Transaction /> },
+      { index: true, element: <UserHomePage /> },
+      { path: "transaction", element: <MakeTransaction /> },
       { path: "history", element: <History /> },
       { path: "profile", element: <UserProfile /> },
     ],
@@ -49,13 +36,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-<<<<<<< HEAD
   return <RouterProvider router={router} />;
-=======
-  
-    return <RouterProvider router={router} />;
-  
->>>>>>> 823ca6338589d616903e1934a7fbd6136b748b31
 }
 
 export default App;
