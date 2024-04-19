7e7f9165dda4e087f65822395d4145b2bd041c1b
Parth Pandya <211290107020p@gmit.edu.in>
2024-04-19 23:29:13 +0530

    add active navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="grid grid-rows-none grid-cols-3 gap-4 h-12 bg-[#222831] text-[#EEEEEE] text-xl p-3">
       <ul className="flex col-span-2 gap-4 bg-[#222831]">
-        <li className=" hover:text-[#00ADB5] bg-[#222831]">
-          <Link className="bg-[#222831]" to={"/"}>
+        <li className=" hover:text-[#3c4545] bg-[#222831]">
+          <NavLink className="bg-[#222831]" to={"/"}>
             XYZ Banking
-          </Link>
+          </NavLink>
         </li>
       </ul>
       <ul className="flex gap-9 flex-row-reverse mr-3 bg-[#222831]">
         <li className="hover:text-[#00ADB5] bg-[#222831]">
-          <Link className="bg-[#222831]" to={"/signup"}>
+          <NavLink className="bg-[#222831]" to={"/signup"}>
             Sign Up
-          </Link>
+          </NavLink>
         </li>
         <li className="hover:text-[#00ADB5] bg-[#222831]">
-          <Link className="bg-[#222831] " to={"/login"}>
+          <NavLink className="bg-[#222831] " to={"/login"}>
             Login
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
