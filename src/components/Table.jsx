63adff2c2babea89c87781ba7b2863a84a8a09b8
Parth Pandya <211290107020p@gmit.edu.in>
2024-04-20 17:02:40 +0530

    add userprofile page

---
+++ src/components/Table.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useTable, useSortBy, usePagination } from 'react-table';
+import styles from '../style/Table.module.css'; // Import styles
+
+const Table = ({ columns, data }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize:4}, // Start from the first page
+    },
+    useSortBy,
+    usePagination
+  );
+
+  return (
+    <div>
+      <table className={styles.table} {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* <div className={styles.pagination}>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+      </div> */}
+    </div>
+  );
+};
+
+export default Table;
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,8 +1,151 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import Table from "./Table";
+import { GoHistory } from "react-icons/go";
+import { NavLink } from "react-router-dom";
+const UserProfile = () => {
+  const fileInputRef = useRef(null);
+  const tableRef = useRef(null);
+  const handleButtonClick = () => {
+    // Trigger a click on the hidden file input element
+    fileInputRef.current.click();
+  };
 
-export default function UserProfile() {
+  const data = React.useMemo(
+    () => [
+      {
+        type: "Received",
+        name: "John",
+        amount: "10000",
+        date: "30/03/2024",
+        time: "5:07 PM",
+      },
+      {
+        type: "Send",
+        name: "Valay",
+        amount: "5000",
+        date: "12/03/2024",
+        time: "4:06 AM",
+      },
+      // Add more data as needed
+    ],
+    []
+  );
+
+  const columns = React.useMemo(
+    () => [
+      { Header: "Type", accessor: "type" },
+      { Header: "Name", accessor: "name" },
+      {
+        Header: "Amount",
+        accessor: "amount",
+        Cell: ({ value, row }) => (
+          <span
+            style={{
+              color: row.original.type === "Received" ? "green" : "red",
+            }}
+          >
+            {value}
+          </span>
+        ),
+      },
+      { Header: "Date", accessor: "date" },
+      { Header: "Time", accessor: "time" },
+      // Add more columns as needed
+    ],
+    []
+  );
+
+  const [showHistory, setShowHistory] = useState(false);
+
+  useEffect(() => {
+    if (showHistory && tableRef.current) {
+      tableRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [showHistory]);
+
+  const handleShowHistoryClick = () => {
+    setShowHistory(true);
+  };
+
   return (
-    <div>UserProfile</div>
-  )
-}
+    <>
+      <div className="rounded-xl max-w-sm mx-auto  overflow-hidden shadow-xl">
+        <div className="text-center my-4 rounded-xl">
+          <button className="mt-4" onClick={handleButtonClick}>
+            <CgProfile className="h-28 w-28" />
+          </button>
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+          />
+          <h3 className="font-serif uppercase mt-3 font-bold text-2xl text-gray-800 dark:text-white mb-1">
+            User Name
+          </h3>
+        </div>
 
+        <div className="sm:divide-y sm:divide-gray-200 rounded-xl ">
+          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <span className="text-sm font-medium text-gray-500">Ac no.</span>
+            <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              123456789{" "}
+            </span>
+          </div>
+          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <span className="text-sm font-medium text-gray-500">
+              Email address
+            </span>
+            <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              p@example.com
+            </span>
+          </div>
+          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <span className="text-sm font-medium text-gray-500">
+              Phone number
+            </span>
+            <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              987456321
+            </span>
+          </div>
+          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <span className="text-sm font-medium text-gray-500">Address</span>
+            <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              123 Main St
+              <br />
+              Anytown, USA 12345
+            </span>
+          </div>
+          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <span className="text-sm font-medium text-gray-500">
+              {" "}
+              Date Of Birth
+            </span>
+            <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              12/3/2004
+            </span>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={handleShowHistoryClick}
+        className="m-auto mt-12 p-2 flex rounded-xl items-center bg-[#222831] hover:opacity-80 text-white"
+      >
+        <GoHistory
+          fill="white"
+          className="bg-[#222831] hover:bg-[#393E46] h-4 mr-2 w-4"
+        />
+        Show Recent Transaction
+      </button>
+      {showHistory && (
+        <div ref={tableRef}>
+          <Table columns={columns} data={data} />
+        </div>
+      )}
+      {showHistory && <NavLink to={"/user/history"}><button className="m-auto  p-2 flex rounded-xl items-center  hover:opacity-80 text-[#222831]">Read More..</button></NavLink>}
+    </>
+  );
+};
+
+export default UserProfile;
