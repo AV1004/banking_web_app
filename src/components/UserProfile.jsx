13ca8da5efe5805812312f507e1ca2ebe27fdf23
Valay Andhariya <andhariyavalay4@gmail.com>
2024-05-08 15:37:06 +0530

    Setting up Account number to 11 digits

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -119,7 +119,7 @@ const UserProfile = () => {
           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <span className="text-sm font-medium text-gray-500">Ac no.</span>
             <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {userDetails.accountNum}
+              {userDetails._id}
             </span>
           </div>
 
