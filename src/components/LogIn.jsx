a7447e1b57d5273c1c2104613034d63e04df6d96
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-24 02:29:44 +0530

    bugs fix in login page

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -42,7 +42,7 @@ export default function LogIn({ loginHandler }) {
             resData.user.address === null ||
             resData.user.dob === null ||
             resData.user.bank === null ||
-            resData.user.upipin === null ||
+            resData.user.upipin === null  ||
             resData.user.image === null
           ) {
             navigate("/user/editprofile");
@@ -52,8 +52,9 @@ export default function LogIn({ loginHandler }) {
         });
       })
       .catch((err) => {
-        toast.error(err);
+        toast.error("user data not found");
         console.log(err);
+        setLoading(false)
       });
   };
 
